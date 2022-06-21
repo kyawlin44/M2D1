baccd2e819d4d309560603ff7c561b3685699ef4
Kyaw Lin <ecomlin44@gmail.com>
2022-06-21 10:40:37 +0200

    Initial commit

---
+++ carbonara.js
@@ -0,0 +1,32 @@
+let spaghetti = 400;
+console.log("SPAGHETTI: " + spaghetti)
+
+let guanciale = 250;
+console.log("GUANCIALE: " + guanciale)
+
+let eggYolks = 6;
+console.log("FRESH EGG YOLKS: " + eggYolks)
+
+let pecorinoRomanoCheese = 50;
+console.log("AGED GRATED PECORINO ROMANO: " + pecorinoRomanoCheese)
+
+let blackPepper = 4;
+console.log("BLACK PAPPER: " + blackPepper)
+
+let bowl = eggYolks + pecorinoRomanoCheese;
+console.log("MEDIUM BOWL (egg yolks & Pecprino Romano's mixture): " + bowl)
+
+let pan = guanciale;
+console.log("PAN (fry guanciale until browned): " + guanciale)
+
+bowl += blackPepper;
+console.log("MEDIUM BOWL (pepper added): " + bowl)
+
+pan += spaghetti;
+console.log("PAN (mixed guanciale with spaghetti): " + pan)
+
+let carbonara = pan + bowl;
+console.log("CARBONARA: " + carbonara)
+
+
+
